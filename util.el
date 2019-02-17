ab020526b48ddbd8400aa3fd9472ff6e6dd8d97f
Hieu Phan <hieupm@coccoc.com>
2019-02-18 01:09:21 +0700

    Add some packages and key bindings

--- util.el
+++ util.el
@@ -11,3 +11,13 @@
     (progn
       (window-configuration-to-register ?_)
       (delete-other-windows))))
+
+;; Comment line or region
+(defun comment-or-uncomment-region-or-line ()
+    "Comments or uncomments the region or the current line if there's no active region."
+    (interactive)
+    (let (beg end)
+        (if (region-active-p)
+            (setq beg (region-beginning) end (region-end))
+            (setq beg (line-beginning-position) end (line-end-position)))
+        (comment-or-uncomment-region beg end)))
